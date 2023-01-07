c6df04ba9cd981c294a7ac308238136087a76405
archae0pteryx <github@pocketcereal.com>
2023-01-07 11:15:02 -0800

    set fish config for gpg

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set GPG_TTY $(tty)
 end
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
