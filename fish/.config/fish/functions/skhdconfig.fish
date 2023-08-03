25f3d4ce2ae7b294325eed924ed862b12149e49f
archae0pteryx <github@pocketcereal.com>
2023-08-03 08:16:16 -0700

    split

---
+++ fish/.config/fish/functions/skhdconfig.fish
@@ -0,0 +1,4 @@
+function skhdconfig --wraps='code ~/.config/skhd' --description 'alias skhdconfig=code ~/.config/skhd'
+  code ~/.config/skhd $argv
+        
+end
---
+++ fish/.config/fish/functions/yabaiconfig.fish
@@ -0,0 +1,4 @@
+function yabaiconfig --wraps='code ~/.config/yabai' --description 'alias yabaiconfig=code ~/.config/yabai'
+  code ~/.config/yabai $argv
+        
+end
